a87faab160e92de2edc096032968ee56099854f2
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-05-06 13:22:28 -0700

    an actual good refactor

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,10 @@
 import { getTerms } from './index'
 
+const checkState = (state, expected) =>
+  expect(getTerms(state)).toEqual(expected)
+
 describe('getTerms', () => {
-  let expected = {
+  const expected = {
     house: 2,
     senate: 4,
     governor: 4
@@ -17,99 +20,35 @@ describe('getTerms', () => {
   })
 
   describe('special cases', () => {
-    it('should return the correct terms for Alabama', () => {
-      const result = getTerms('AL')
-      expect(result).toEqual({ ...expected, house: 4 })
+    describe('states with two year senate terms', () => {
+      const states = ['AZ', 'CT', 'GA', 'ID', 'MA', 'ME', 'NC', 'NY', 'RI', 'SD']
+
+      it('should return the correct terms for relevant states', () => {
+        states.map(state => checkState(state, { ...expected, senate: 2 }))
+      })
     })
 
-    it('should return the correct terms for Arizona', () => {
-      const result = getTerms('AZ')
-      expect(result).toEqual({ ...expected, senate: 2 })
+    describe('states with four year house terms', () => {
+      const states = ['AL', 'LA', 'MD', 'MO', 'MS', 'ND']
+
+      it('should return the correct terms for relevant states', () => {
+        states.map(state => checkState(state, { ...expected, house: 4 }))
+      })
     })
 
-    it('should return the correct terms for Conneticut', () => {
-      const result = getTerms('CT')
-      expect(result).toEqual({ ...expected, senate: 2 })
+    describe('states with two year senate and governor terms', () => {
+      const states = ['NH', 'VT']
+
+      it('should return the correct terms for relevant states', () => {
+        states.map(state => checkState(state, { ...expected, senate: 2, governor: 2 }))
+      })
     })
 
-    it('should return the correct terms for Georgia', () => {
-      const result = getTerms('GA')
-      expect(result).toEqual({ ...expected, senate: 2 })
-    })
-
-    it('should return the correct terms for Idaho', () => {
-      const result = getTerms('ID')
-      expect(result).toEqual({ ...expected, senate: 2 })
-    })
-
-    it('should return the correct terms for Louisiana', () => {
-      const result = getTerms('LA')
-      expect(result).toEqual({ ...expected, house: 4 })
-    })
-
-    it('should return the correct terms for Maine', () => {
-      const result = getTerms('ME')
-      expect(result).toEqual({ ...expected, senate: 2 })
-    })
-
-    it('should return the correct terms for Maryland', () => {
-      const result = getTerms('MD')
-      expect(result).toEqual({ ...expected, house: 4 })
-    })
-
-    it('should return the correct terms for Massachusets', () => {
-      const result = getTerms('MA')
-      expect(result).toEqual({ ...expected, senate: 2 })
-    })
-
-    it('should return the correct terms for Mississippi', () => {
-      const result = getTerms('MS')
-      expect(result).toEqual({ ...expected, house: 4 })
-    })
-
-    it('should return the correct terms for Missouri', () => {
-      const result = getTerms('MO')
-      expect(result).toEqual({ ...expected, house: 4 })
-    })
-
-    it('should return the correct terms for Nebraska', () => {
-      const result = getTerms('NE')
-      expect(result).toEqual({ ...expected, house: undefined })
-    })
-
-    it('should return the correct terms for New Hampshire', () => {
-      const result = getTerms('NH')
-      expect(result).toEqual({ ...expected, senate: 2, governor: 2 })
-    })
-
-    it('should return the correct terms for New York', () => {
-      const result = getTerms('NY')
-      expect(result).toEqual({ ...expected, senate: 2 })
-    })
-
-    it('should return the correct terms for North Carolina', () => {
-      const result = getTerms('NC')
-      expect(result).toEqual({ ...expected, senate: 2 })
-    })
-
-    it('should return the correct terms for North Dakota', () => {
-      const result = getTerms('ND')
-      expect(result).toEqual({ ...expected, house: 4 })
-    })
-
-    it('should return the correct terms for Rhode Island', () => {
-      const result = getTerms('RI')
-      expect(result).toEqual({ ...expected, senate: 2 })
-    })
-
-    it('should return the correct terms for South Dakota', () => {
-      const result = getTerms('SD')
-      expect(result).toEqual({ ...expected, senate: 2 })
-    })
-
-    it('should return the correct terms for Vermont', () => {
-      const result = getTerms('VT')
-      expect(result).toEqual({ ...expected, senate: 2, governor: 2 })
+    describe('a unique snowflake', () => {
+      it('should return the correct terms for Nebraska', () => {
+        const result = getTerms('NE')
+        expect(result).toEqual({ ...expected, house: undefined })
+      })
     })
   })
 })
